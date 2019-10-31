cf50531c917056d9127b50be5d4b5e938466f711
bywdsg <liuyao928@tom.com>
2019-10-31 21:42:35 +1000

    step1 use map to render component

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,33 +1,60 @@
 import React from 'react';
 import ForecastRow from './ForecastRow';
 
-function WeatherForecast() {
-    return (
-        <section className="weather-forecast">
-            <div className="forecast__switch">
-                <button className="forecast__switch_0 switch-active">5 items</button>
-                <button className="forecast__switch_1">10 items</button>
-            </div>
+class WeatherForecast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            forecasts: [
+                {
+                    day: "Fri",
+                    high: "19 c",
+                    low: "8 c",
+                    time: "10:00"
+                },
+                {
+                    day: "Sat",
+                    high: "19 c",
+                    low: "8 c",
+                    time: "10:00"
+                },
+                {
+                    day: "Sun",
+                    high: "19 c",
+                    low: "8 c",
+                    time: "10:00"
+                },
+                {
+                    day: "Mon",
+                    high: "19 c",
+                    low: "8 c",
+                    time: "10:00"
+                }
+            ]
 
-            <ForecastRow
-                day="Fri"
-                high="19 c"
-                low="8 c"
-                time="10:00"
-            />
-            <ForecastRow
-                day="Fri"
-                high="19 c"
-                low="8 c"
-                time="13:00"
-            />
-            <ForecastRow
-                day="Fri"
-                high="19 c"
-                low="8 c"
-                time="16:00"
-            />
-        </section>
-    );
+        };
+    }
+    render() {
+        return (
+            <section className="weather-forecast">
+                <div className="forecast__switch">
+                    <button className="forecast__switch_0 switch-active">5 items</button>
+                    <button className="forecast__switch_1">10 items</button>
+                </div>
+                {this.state.forecasts.map(forecast => (
+                    <ForecastRow
+                        key={forecast.day + forecast.time}
+                        day={forecast.day}
+                        high={forecast.high}
+                        low={forecast.low}
+                        time={forecast.time}
+                    />
+                ))}
+
+
+            </section>
+        );
+    }
+
 }
 export default WeatherForecast;
